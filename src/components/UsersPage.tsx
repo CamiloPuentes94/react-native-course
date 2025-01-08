00663dc28280160dc1655243196184c016b4fb5c
camilo puentes <jcpsandoval94@gmail.com>
2025-01-08 10:30:39 -0500

    add UsersPage component to fetch and display user data

---
+++ src/components/UsersPage.tsx
@@ -0,0 +1,50 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { ReqResUserListResponse } from "../interfaces"
+
+const loadUsers = async () => {
+  try {
+    const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users')
+    return data.data
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+
+}
+
+export const UsersPage = () => {
+
+  useEffect(() => {
+
+    loadUsers().then(user => console.log(user))
+
+  }, [])
+
+
+  return (
+    <>
+      <h3>Usuarios:</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nombre</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              avatar
+            </td>
+            <td>nombre</td>
+            <td>
+              email
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
